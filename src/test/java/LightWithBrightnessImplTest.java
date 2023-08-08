4d43f76fc208a279f6b4f2b5ec093b68a53535d9
Thrisha <thrisha.bandi@zemosolabs.com>
2023-08-08 19:40:07 +0530

    Test code refactoring using before each

--- src/test/java/LightWithBrightnessImplTest.java
+++ src/test/java/LightWithBrightnessImplTest.java
@@ -1,32 +1,35 @@
 import org.example.impl.LightImpl;
 import org.example.impl.LightWithBrightnessImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LightWithBrightnessImplTest {
+
+    LightWithBrightnessImpl light;
+    @BeforeEach
+    void setup(){
+        light = new LightWithBrightnessImpl();
+    }
     @Test
     void testLightInitialBrightness(){
-        LightWithBrightnessImpl light = new LightWithBrightnessImpl();
         Assertions.assertEquals(0,light.getBrightness());
     }
 
     @Test
     void testLightTurnOn(){
-        LightWithBrightnessImpl light = new LightWithBrightnessImpl();
         int brightnessBeforeTurnOn = light.getBrightness();
         light.turnOn();
         Assertions.assertEquals(brightnessBeforeTurnOn+1, light.getBrightness());
     }
     @Test
     void testLightTurnOffInitiallyWhenAlreadyOff(){
-        LightWithBrightnessImpl light = new LightWithBrightnessImpl();
         light.turnOff();
         Assertions.assertEquals(0, light.getBrightness());
     }
 
     @Test
     void testLightTurnOff(){
-        LightWithBrightnessImpl light = new LightWithBrightnessImpl();
         light.turnOn();
         int brightnessBeforeTurnOff = light.getBrightness();
         light.turnOff();
@@ -35,10 +38,8 @@ public class LightWithBrightnessImplTest {
 
     @Test
     void testLightToggle() {
-        LightWithBrightnessImpl light = new LightWithBrightnessImpl();
         int brightnessBeforeToggle = light.getBrightness();
         light.toggle();
         Assertions.assertEquals(brightnessBeforeToggle + 2, light.getBrightness());
-
     }
 }
